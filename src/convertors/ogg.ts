4850b65eb2973c8bb91f192932b23443fa020b01
denissb <spawnenius@gmail.com>
2023-05-18 15:12:30 +0200

    downloading ogg files from telegram

---
+++ src/convertors/ogg.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { createWriteStream } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+class OggConverter {
+    contructor() {}
+
+    toMp3() {}
+
+    async create(url: string, filename: string) {
+        try {
+            const oggPath = resolve(__dirname, '../../tmp/audio', `${filename}.ogg`);
+            const response = await axios({
+                method: 'get',
+                url,
+                responseType: 'stream'
+            });
+            return new Promise((resolve, reject) => {
+                const stream = createWriteStream(oggPath);
+                response.data.pipe(stream);
+                stream.on('finish', () => resolve(oggPath));
+                stream.on('error', () => reject(new Error('Error reading from stream')));
+            });
+        } catch (e) {
+            console.error('Failed downloading ogg message file');
+        }
+    }
+}
+
+export const ogg = new OggConverter();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
-function sayMyName(name: string): void {
-    if (name === "Heisenberg") {
-      console.log("You're right 👍");
-    } else {
-      console.log("You're wrong 👎");
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+import config from 'config';
+import { ogg } from './convertors/ogg';
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
+
+bot.command('start', async (ctx) => {
+    await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
+
+bot.on(message('text'), async ctx => {
+    await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
+
+bot.on(message('voice'), async ctx => {
+    try {
+        // await ctx.reply(JSON.stringify(ctx.message.voice, null, 2));
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+        const userId =  ctx.message.from.id;
+        const oggPath = await ogg.create(link.href, String(userId));
+    } catch (e) {
+        console.error('Error on voice message', e);
     }
-  }
-   
-  sayMyName("Heisenberg");
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
